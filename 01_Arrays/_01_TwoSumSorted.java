7dcc93c3daadb2ca32289bb6d7bd0fdbaf9ef548
Mohit Malik <malikmohit332@gmail.com>
2026-04-25 20:02:41 +0530

    _01_TwoSumSorted (Two Pointers - Opposite Direction)

---
+++ 01_Arrays/_01_TwoSumSorted.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+public class _01_TwoSumSorted {
+    public static void main(String[] args){
+        int[] nums = {2,7,11,15};
+        int target = 9;
+
+        System.out.println(Arrays.toString(twoSum(nums, target)));
+    }
+
+    public static int[] twoSum(int[] nums, int target){
+        int left = 0;
+        int right = nums.length - 1;
+
+        while (left < right){
+            int sum = nums[left] + nums[right];
+
+            if (sum == target){
+                return new int[]{left, right};
+            }
+            else if (sum < target){
+                left++;
+            }
+            else{
+                right--;
+            }
+        }
+
+        return new int[]{-1, -1};
+    }
+}
